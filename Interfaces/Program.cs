e0e2fffca97dc07dab3a9ccfd35662e0ab5f3e8e
David Offei Akuffo <david.akuffo11@gmail.com>
2024-06-17 21:42:29 +0000

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿// Interface
+public interface IMovable
+{
+    void Move();
+}
+
+// Class Car
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Class Bicycle
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        car.Move();      // Outputs: Car is moving
+        bicycle.Move();  // Outputs: Bicycle is moving
+    }
+}
+
