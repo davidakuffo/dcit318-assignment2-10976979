dc70427c64a9cff6755d026808c8b3d75861da75
David Offei Akuffo <david.akuffo11@gmail.com>
2024-06-17 21:32:50 +0000

    Add project files.

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,41 @@
+﻿// Base class
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class Dog
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class Cat
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Animal myAnimal = new Animal();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+        myAnimal.MakeSound(); // Outputs: Some generic sound
+        myDog.MakeSound();    // Outputs: Bark
+        myCat.MakeSound();    // Outputs: Meow
+    }
+}
+
